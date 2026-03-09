afeaf4bd486bbf06656acd3e0191ccf93448faf2
EduardoEricDev <105312728+EduardoEricDev@users.noreply.github.com>
2026-03-09 15:46:24 -0300

    Feat/setup server express
    * chore: init typscript
    
    * feat: initial server setup with express

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from "express";
+
+const router = Router();
+
+// Rota de teste
+// router.get("/teste", (request: Request, response: Response) => {
+// 	return response.json({ ok: true });
+// });
+
+export { router };
