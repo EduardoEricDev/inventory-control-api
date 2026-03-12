5f66ed59ce90731ebd98c7148a1edc421d21c60b
EduardoEricDev <eduardo.dev2022@gmail.com>
2026-03-12 15:23:44 -0300

    refactor: switch query for routes params

--- src/controllers/category/EditCategoryController.ts
+++ src/controllers/category/EditCategoryController.ts
@@ -3,17 +3,19 @@ import { EditCategoryService } from "../../services/category/EditCategoryService
 import { EditCategorySchema } from "../../schemas";
 
 class EditCategoryController {
-    async handle(req: Request, res: Response) {
+	async handle(req: Request, res: Response) {
+		const { name, category_id } = EditCategorySchema.parse({
+			name: req.body.name,
+			category_id: req.params.category_id,
+		});
+		const editCategoryService = new EditCategoryService();
+		const categoryEdited = await editCategoryService.execute({
+			name,
+			category_id,
+		});
 
-        const { name, category_id } = EditCategorySchema.parse({
-            name: req.body.name,
-            category_id: req.query.category_id,
-        })
-        const editCategoryService = new EditCategoryService();
-        const categoryEdited = await editCategoryService.execute({name, category_id});
-
-        return res.json(categoryEdited);
-    }
+		return res.json(categoryEdited);
+	}
 }
 
 export { EditCategoryController };
