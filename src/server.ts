a29ef357d83f17a727ed80a8da1560c888ec8361
EduardoEricDev <eduardo.dev2022@gmail.com>
2026-03-22 17:15:47 -0300

    chore: setup swagger ui

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
 import "express-async-errors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 import { router } from "./routes";
 import { errorHandler } from "./middlewares/error-handler";
 import path from "path";
@@ -8,11 +11,23 @@ const app = express();
 const port = 3333;
 
 app.use(express.json());
-app.use(router);
+app.use(cors());
 
+// Documentação Swagger (Acessível em http://localhost:3333/api/docs)
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Rotas da API com prefixo de versão
+app.use("/v1", router);
+
+// Servir arquivos estáticos (Banners dos produtos)
 app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
+// Middleware de Erro
 app.use(errorHandler);
+
+app.get("/terms", (req: Request, res: Response) => {
+	return res.json({ message: "Termos de serviço" });
+});
 
 app.listen(port, () => {
 	console.log(`🚀 Servidor rodando na porta ${port} - Inventory Control API`);
