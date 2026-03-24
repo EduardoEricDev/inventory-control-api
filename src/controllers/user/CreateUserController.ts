8fc2a4d6adb8a756b0e2d665f8a435fdf9cc2cc7
EduardoEricDev <eduardo.dev2022@gmail.com>
2026-03-23 22:14:31 -0300

    docs: implement user registration path, schemas and fix api status codes

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -9,7 +9,7 @@ class CreateUserController {
 		const createUserService = new CreateUserService();
 		const user = await createUserService.execute({ name, email, password });
 
-		return res.json(user);
+		return res.status(201).json(user);
 	}
 }
 
